aea2c6bb12500381e6d29bbf11922174f139a16e
Nilesh Laxmi <nileshlaxmi4@gmail.com>
2018-09-22 23:21:49 +0530

    Working on Homapafe animation

--- js/app.js
+++ js/app.js
@@ -1,3 +1,19 @@
 function hireButton(){
     window.location.href = "contact.html";
 }
+var images = ["bg-one.jpg", "bg-two.jpg", "bg-three.jpg"];
+
+$(function(){
+    let i = 0;
+    $("#homeImage").css("background-image", "url(images/" + images[i] + ")");
+        setInterval(function () {
+            i++;
+            if (i == images.length) {
+                i = 0;
+            }
+            $("#homeImage").fadeOut("slow", function () {
+                $(this).css("background-image", "url(images/" + images[i] + ")");
+                $(this).fadeIn("slow");
+            });
+        }, 5000);
+});
