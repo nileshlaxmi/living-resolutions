92347b349b6c2c54fa9303f5a526d5da1e6b4ed8
Nilesh Laxmi <nileshlaxmi4@gmail.com>
2018-11-13 01:04:38 +0530

    Updated path and image src

--- js/app.js
+++ js/app.js
@@ -5,14 +5,16 @@ var images = ["bg-one.jpg", "bg-two.jpg", "bg-three.jpg"];
 
 $(function(){
     let i = 0;
-    $("#homeImage").css("background-image", "url(images/" + images[i] + ")");
+    $("#homeImage").css("background-image", "url(\"../../images/" + images[i] + "\"");
+        console.log("images", images);
         setInterval(function () {
             i++;
             if (i == images.length) {
                 i = 0;
             }
             $("#homeImage").fadeOut("slow", function () {
-                $(this).css("background-image", "url(images/" + images[i] + ")");
+                console.log("background-image", "url(\"../../images/" + images[i] + "\")");
+                $(this).css("background-image", "url(\"../../images/" + images[i] + "\")");
                 $(this).fadeIn("slow");
             });
         }, 5000);
