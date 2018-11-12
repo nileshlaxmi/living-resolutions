f4a62a7293f09f1100d83b9effae90eaec77d1fc
Nilesh Laxmi <nileshlaxmi4@gmail.com>
2018-11-13 01:26:26 +0530

    Fixed src issue

--- js/app.js
+++ js/app.js
@@ -5,6 +5,8 @@ var images = ["images/bg-one.jpg", "images/bg-two.jpg", "images/bg-three.jpg"];
 
 $(function(){
     let i = 0;
+    var currentImg;
+
     // $("#homeImage").css("background-image", "url(images/" + images[i] + ")");
     //     setInterval(function () {
     //         i++;
@@ -18,30 +20,20 @@ $(function(){
     //     }, 5000);
 
     for(i=0; i< images.length; i++){
-        var currentImg = images[i];
+        currentImg = images[i];
     }
 
-    $("#homepage").animate({opacity: 0}, slow, function(){
-        $(this).attr(src, currentImg).animate({opacity:1});
+    $("#homepage").animate({opacity: 0}, "slow", function(){
+        $(this).attr("src", currentImg).animate({opacity:1});
     });
 
     $("#homepage").change(function(){
         var selected = $(this).val();
         var image = $("#homepage");
-        image.fadeOut('fast', function () {
-            image.attr('src', '/images/'+selected+'.jpg');
+        let path = '/images/'+selected+'.jpg';
+        image.fadeOut('slow', function () {
+            image.attr("src", path);
             image.fadeIn('fast');
         });
     });
-
-
-    // $("#vehicle").change(function(){
-    //     var selected = $(this).val();
-    //     var image = $("#selectedVehicle");
-    //     image.fadeOut('fast', function () {
-    //         image.attr('src', '/assets/images/mini/'+selected+'.png');
-    //         image.fadeIn('fast');
-    //     });
-    // });
-
 });
