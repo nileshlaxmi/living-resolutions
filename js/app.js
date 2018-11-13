fd7a878e99145a173109180fe22ac3d77e182620
Nilesh Laxmi <nileshlaxmi4@gmail.com>
2018-11-14 00:46:29 +0530

    Added animation for home page image

--- js/app.js
+++ js/app.js
@@ -6,34 +6,21 @@ var images = ["images/bg-one.jpg", "images/bg-two.jpg", "images/bg-three.jpg"];
 $(function(){
     let i = 0;
     var currentImg;
-
-    // $("#homeImage").css("background-image", "url(images/" + images[i] + ")");
-    //     setInterval(function () {
-    //         i++;
-    //         if (i == images.length) {
-    //             i = 0;
-    //         }
-    //         $("#homeImage").fadeOut("slow", function () {
-    //             $(this).css("background-image", "url(images/" + images[i] + ")");
-    //             $(this).fadeIn("slow");
-    //         });
-    //     }, 5000);
-
-    for(i=0; i< images.length; i++){
+    setInterval(function(){
+        // document.getElementById("homepage").src = images[1];
         currentImg = images[i];
-    }
 
-    $("#homepage").animate({opacity: 0}, "slow", function(){
-        $(this).attr("src", currentImg).animate({opacity:1});
-    });
-
-    $("#homepage").change(function(){
-        var selected = $(this).val();
-        var image = $("#homepage");
-        let path = '/images/'+selected+'.jpg';
-        image.fadeOut('slow', function () {
-            image.attr("src", path);
-            image.fadeIn('fast');
+        $("#homepage").animate({opacity: 0}, "slow", function(){
+            $(this).attr("src", currentImg).animate({opacity:0.7});
         });
-    });
+
+        if(i>=2){
+            i=0;
+        }
+        else{
+            i++;
+        }
+
+        console.log("i", i , " currentImg", currentImg);
+    }, 3500);
 });
