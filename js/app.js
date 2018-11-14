5874a9d1c52b4396e40afcad7fb872adc0850923
Nilesh Laxmi <nileshlaxmi4@gmail.com>
2018-11-14 22:22:28 +0530

    Tried Tobi

--- js/app.js
+++ js/app.js
@@ -1,7 +1,12 @@
+var tobi = new Tobi({
+    close: true,
+    keyboard: true
+});
+
 function hireButton(){
     window.location.href = "contact.html";
 }
-var images = ["images/bg-one.jpg", "images/bg-two.jpg", "images/bg-three.jpg"];
+var images = ["images/bg-two.jpg", "images/bg-three.jpg", "images/bg-one.jpg"];
 
 $(function(){
     let i = 0;
@@ -11,7 +16,7 @@ $(function(){
         currentImg = images[i];
 
         $("#homepage").animate({opacity: 0}, "slow", function(){
-            $(this).attr("src", currentImg).animate({opacity:0.7});
+            $(this).attr("src", currentImg).animate({opacity:0.6});
         });
 
         if(i>=2){
@@ -24,14 +29,5 @@ $(function(){
         console.log("i", i , " currentImg", currentImg);
     }, 3500);
 
-    $('.gallery-item').magnificPopup({
-        type: 'image',
-        gallery:{
-            enabled:true,
-            navigateByImgClick: true,
-            tPrev: 'Previous (Left arrow key)', // title for left button
-            tNext: 'Next (Right arrow key)', // title for right button
-            tCounter: '<span class="mfp-counter">%curr% of %total%</span>' // markup of counter
-        }
-    });
+    
 });
